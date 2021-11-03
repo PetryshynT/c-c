be94c644c269f4f4f93afdcd7085272915a84eaa
PetryshynT <93601436+PetryshynT@users.noreply.github.com>
2021-11-03 19:24:27 +0200

    without comments

--- lesson2/comparisons.js
+++ lesson2/comparisons.js
@@ -10,35 +10,35 @@
 ? ternary operation/коли потрібно порівняти значення змінної і в залежності від рехультату видати ту чи іншу операцію
 */
 
-// const MATH_PI = 3.14;
-// const radius = 10;
-// const name2 = "Robert";
-//  console.log(MATH_PI === 3.14);
-//  console.log(name2 === "robert");
-//  console.log(name2 == "robert");
-//  console.log(name2 != "robert");
-//  console.log(name2 != "Robert");
-//  console.log(name2 === "Robert");
-// console.log(radius > 10);
-// console.log(radius >= 10);
-// console.log(radius < 10);
-// console.log(radius > 100);
-// console.log(radius !== 0);
+const MATH_PI = 3.14;
+const radius = 10;
+const name2 = "Robert";
+ console.log(MATH_PI === 3.14);
+ console.log(name2 === "robert");
+ console.log(name2 == "robert");
+ console.log(name2 != "robert");
+ console.log(name2 != "Robert");
+ console.log(name2 === "Robert");
+console.log(radius > 10);
+console.log(radius >= 10);
+console.log(radius < 10);
+console.log(radius > 100);
+console.log(radius !== 0);
 
-// const MATH_PI = 3.14;
-// const radius = 10;
-// const formula = 'circumference';
-// const result = formula === 'circumference'
-// ? MATH_PI * ( 2* radius)
-// : MATH_PI * (radius * radius);
-// console.log(result); 
+const MATH_PI = 3.14;
+const radius = 10;
+const formula = 'circumference';
+const result = formula === 'circumference'
+? MATH_PI * ( 2* radius)
+: MATH_PI * (radius * radius);
+console.log(result); 
 
-// const requiredHight = 145;
-// let hight = 150;
-// const welcome = "Welcome to the attraction ";
-// const cancel = "Come next time"
-// const result1 = hight >= requiredHight ? welcome : cancel; 
-// console.log(result1);
+const requiredHight = 145;
+let hight = 150;
+const welcome = "Welcome to the attraction ";
+const cancel = "Come next time"
+const result1 = hight >= requiredHight ? welcome : cancel; 
+console.log(result1);
 
 
 
