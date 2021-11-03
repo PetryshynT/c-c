275e8bda7cf3fba1a45968cf9170a58a7984ce4c
PetryshynT <93601436+PetryshynT@users.noreply.github.com>
2021-11-03 19:22:58 +0200

    with compleated baby chair task

--- lesson2/comparisons.js
+++ lesson2/comparisons.js
@@ -25,11 +25,33 @@
 // console.log(radius > 100);
 // console.log(radius !== 0);
 
-const MATH_PI = 3.14;
-const radius = 10;
-const formula = 'circumference';
-const result = formula === 'circumference'
-? MATH_PI * ( 2* radius)
-: MATH_PI * (radius * radius);
-console.log(result); 
+// const MATH_PI = 3.14;
+// const radius = 10;
+// const formula = 'circumference';
+// const result = formula === 'circumference'
+// ? MATH_PI * ( 2* radius)
+// : MATH_PI * (radius * radius);
+// console.log(result); 
 
+// const requiredHight = 145;
+// let hight = 150;
+// const welcome = "Welcome to the attraction ";
+// const cancel = "Come next time"
+// const result1 = hight >= requiredHight ? welcome : cancel; 
+// console.log(result1);
+
+
+
+
+const requiredHight = 145;
+const requiredAge = 12;
+let presentAge = 10;
+let presentHight =135;
+/*не працює
+ let desicion = presentAge, presentHight >= requiredHight || requiredAge ? "You dont need baby chair" : "You need baby chair";
+console.log(desicion);
+*/
+const goodHight = presentHight >= requiredHight ? true : false;
+const goodAge = presentAge >= requiredAge ? true : false;
+const desicion = goodHight || goodAge == true ? "don't need baby chair" : "need baby cgair";
+console.log(desicion);
--- lesson2/operators.js
+++ lesson2/operators.js
@@ -1,24 +1,24 @@
-// const MATH_PI = 3.14;
-// const radius = 10;
-// // const result = MATH_PI * radius * radius;
-// // console.log(result);
+const MATH_PI = 3.14;
+const radius = 10;
+const result = MATH_PI * radius * radius;
+console.log(result);
 
 // // shorter way
-// let result = 10 * 10;
-// result *= MATH_PI;
-// // console.log(result);
+let result = 10 * 10;
+result *= MATH_PI;
+console.log(result);
 
 //зменшення значення
 const a = 10;
 const b = 30;
 let c = 60;
 
-// c = c - a;
+c = c - a;
 //shorter way
-// c -= a; // may read like c=c-a
-// console.log(c);
-// c += a;
-// c += b;
+c -= a; // may read like c=c-a
+console.log(c);
+c += a;
+c += b;
 
 // string operation
 let nameU = "John";
