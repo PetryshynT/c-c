388a81f046386f1bf147ceb162ef4cbde43cdcca
PetryshynT <93601436+PetryshynT@users.noreply.github.com>
2021-11-03 12:32:32 +0200

    comments created

---
+++ lesson2/coments.js
@@ -0,0 +1,14 @@
+// I can add a comment like that
+const myVar ='test';
+
+/*
+I can add a comment also like that
+to have multilines comment
+
+*/
+const myComment = "multiline comment";
+
+/*
+Code below is function that should find a sum of 2 elements
+*/
+const sum = (a, b) => a +b;
