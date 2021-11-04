24cfa8acc4b3356d4d1fd0c4973d4c8694624bc8
PetryshynT <93601436+PetryshynT@users.noreply.github.com>
2021-11-04 17:10:20 +0200

    updated

---
+++ lesson2/switch.js
@@ -0,0 +1,143 @@
+// switch case - коли є вираз і безліч варіантів кейсів, а в кейсаї є свої інструкції
+
+// const day = 8;
+
+// switch (day) {
+//     case 1: {
+//         console.log("Mondey");
+//     }
+//     break;
+
+//     case 2: {
+//         console.log("Tuesday");
+//     }
+//     break;
+
+//     case 3: {
+//         console.log("Wdnesday");
+//     }
+//     break;
+
+//     case 4: {
+//         console.log("Thursday");
+//     }
+//     break
+
+//     case 5: {
+//         console.log("Friday");
+//     }
+//     break
+
+//     case 6: {
+//         console.log("Saturday");
+//     }
+//     break;
+
+//     case 7: {
+//         console.log("Sunday");
+//     }
+//     break;
+//     default: {
+//         console.log("in week only seven days")
+//     }
+
+// }
+
+
+
+// const month = 113;
+// switch (month) {
+//     case 1: {
+//         console.log("Junuary");
+//     }
+//     break;
+    
+//     case 2: {
+//         console.log("Februrary");
+//     }
+//     break;
+   
+//     case 3: {
+//         console.log("March");
+//     }
+//     break;
+   
+//     case 4: {
+//         console.log("April");
+//     }
+//     break;
+   
+//     case 5: {
+//         console.log("May");
+//     }
+//     break;
+   
+//     case 6: {
+//         console.log("June");
+//     }
+//     break;
+    
+//     case 7: {
+//         console.log("July");
+//     }
+//     break;
+//     case 8: {
+//         console.log("August");
+//     }
+//     break;
+   
+//     case 9: {
+//         console.log("September");
+//     }
+//     break;
+    
+//     case 10: {
+//         console.log("October");
+//     }
+//     break;
+    
+//     case 11: {
+//         console.log("November");
+//     }
+//     break;
+    
+//     case 12: {
+//         console.log("December");
+//     }
+//     break;
+    
+//     default: {
+//         console.log("We have only 12 months, please enter number from 1 to 10")
+//     }
+// }
+
+
+// const userName = "Taras";
+// switch (userName) {
+//     case "Taras": {
+//         console.log("Hello Taras");
+//     }
+//     break;
+//     default: {
+//         console.log("I don't know you, sorry it's only buisinnes")
+//     }
+// }
+
+const userName = "2324";
+const username1 = "2";
+switch (userName && username1) {
+    case "Oleh": {
+        console.log("Hello");
+    }
+    case "Taras": {
+        console.log("Hello");
+    }
+    break;
+    case "" : {
+        console.log("Please enter your name");
+    }
+    break;
+    default: {
+        console.log("I don't know you, sorry it's only buisinnes")
+    }
+}
