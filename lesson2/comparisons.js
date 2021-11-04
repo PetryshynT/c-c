516fca1e6ac6a1c1da3979ddea88160bdfe28434
PetryshynT <93601436+PetryshynT@users.noreply.github.com>
2021-11-04 18:29:46 +0200

    updated

--- lesson2/comparisons.js
+++ lesson2/comparisons.js
@@ -10,48 +10,53 @@
 ? ternary operation/коли потрібно порівняти значення змінної і в залежності від рехультату видати ту чи іншу операцію
 */
 
-const MATH_PI = 3.14;
-const radius = 10;
-const name2 = "Robert";
- console.log(MATH_PI === 3.14);
- console.log(name2 === "robert");
- console.log(name2 == "robert");
- console.log(name2 != "robert");
- console.log(name2 != "Robert");
- console.log(name2 === "Robert");
-console.log(radius > 10);
-console.log(radius >= 10);
-console.log(radius < 10);
-console.log(radius > 100);
-console.log(radius !== 0);
+// const MATH_PI = 3.14;
+// const radius = 10;
+// const name2 = "Robert";
+//  console.log(MATH_PI === 3.14);
+//  console.log(name2 === "robert");
+//  console.log(name2 == "robert");
+//  console.log(name2 != "robert");
+//  console.log(name2 != "Robert");
+//  console.log(name2 === "Robert");
+// console.log(radius > 10);
+// console.log(radius >= 10);
+// console.log(radius < 10);
+// console.log(radius > 100);
+// console.log(radius !== 0);
 
-const MATH_PI = 3.14;
-const radius = 10;
-const formula = 'circumference';
-const result = formula === 'circumference'
-? MATH_PI * ( 2* radius)
-: MATH_PI * (radius * radius);
-console.log(result); 
+// const MATH_PI = 3.14;
+// const radius = 10;
+// const formula = 'circumference';
+// const result = formula === 'circumference'
+// ? MATH_PI * ( 2* radius)
+// : MATH_PI * (radius * radius);
+// console.log(result); 
 
-const requiredHight = 145;
-let hight = 150;
-const welcome = "Welcome to the attraction ";
-const cancel = "Come next time"
-const result1 = hight >= requiredHight ? welcome : cancel; 
-console.log(result1);
+// const requiredHight = 145;
+// let hight = 150;
+// const welcome = "Welcome to the attraction ";
+// const cancel = "Come next time"
+// const result1 = hight >= requiredHight ? welcome : cancel; 
+// console.log(result1);
 
 
 
 
 const requiredHight = 145;
 const requiredAge = 12;
-let presentAge = 10;
-let presentHight =135;
-/*не працює
- let desicion = presentAge, presentHight >= requiredHight || requiredAge ? "You dont need baby chair" : "You need baby chair";
+let presentAge = 1;
+let presentHight =150;
+
+ let desicion = presentAge > requiredAge && presentHight > requiredHight ? "You dont need baby chair" : "You need baby chair";
 console.log(desicion);
-*/
-const goodHight = presentHight >= requiredHight ? true : false;
-const goodAge = presentAge >= requiredAge ? true : false;
-const desicion = goodHight || goodAge == true ? "don't need baby chair" : "need baby cgair";
-console.log(desicion);
+
+// const goodHight = presentHight >= requiredHight ? true : false;
+// const goodAge = presentAge >= requiredAge ? true : false;
+// const desicion = goodHight || goodAge == true ? "don't need baby chair" : "need baby cgair";
+// console.log(desicion);
+
+// let desicion = presentAge || requiredAge && presentHight >= requiredHight ? "You dont need baby chair" : "You need baby chair";
+// console.log(desicion);
+// let desicion = presentAge || presentHight > requiredAge || requiredHight ? "You dont need baby chair" : "You need baby chair";
+// console.log(desicion);
